5ba5970ef1e6fded47ca42afa57ab7f333a18175
CauevSilv <caue.silva10@fatec.sp.gov.br>
2024-09-06 21:01:21 -0300

    :sparkles: add base plugins
    Added base plugins for the client application.
    PrimeVue
    Axios
    OpenLayers

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//OpenLayers plugin import
+import "vue3-openlayers/styles.css";
+import OpenLayersMap from "vue3-openlayers";
+
+//PrimeVue plugin import
+import PrimeVue from "primevue/config";
+
 const app = createApp(App)
+
+app.use(PrimeVue, {
+    unstyled: true
+});
+
+app.use(OpenLayersMap);
 
 app.use(router)
 
