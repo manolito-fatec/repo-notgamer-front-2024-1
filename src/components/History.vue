9e57525397d4c945cea5a78d738945d2147c86ec
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2024-09-25 19:51:37 -0300

    💄style: font fix
    Arrumei a fonte do texto

--- src/components/History.vue
+++ src/components/History.vue
@@ -14,6 +14,9 @@ const historyText = ref('')
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+
 .history-wrapper {
   margin-top: 16px;
 }
@@ -43,7 +46,7 @@ const historyText = ref('')
   color: #ffffff;
   padding: 16px;
   font-size: 16px;
-  font-family: Arial, sans-serif;
+  font-family: 'Poppins', regular, sans-serif;
   resize: none;
   box-sizing: border-box;
   cursor: default;
--- src/views/GeoFilterView.vue
+++ src/views/GeoFilterView.vue
@@ -97,8 +97,8 @@ function handleReset() {
 
 
 <style scoped>
-
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 .filters {
   position: fixed;
   top: 0;
@@ -115,11 +115,6 @@ function handleReset() {
   overflow-y: auto;
   transition: left 0.5s ease;
   font-family: 'Poppins', regular, sans-serif;
-}
-.poppins-regular {
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  font-style: normal;
 }
 
 .button-group {
