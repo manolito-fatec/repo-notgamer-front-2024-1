4d004abcc2b6ee3d14a63291d0d8d1213d0f7ff1
ocalderan <otaviocalderanadm@gmail.com>
2024-10-14 20:19:12 -0300

    ⚰️ (remove): Remove css from warning message

--- src/views/GeoFilterView.vue
+++ src/views/GeoFilterView.vue
@@ -43,7 +43,7 @@ import History from "@/components/History.vue";
 import ClearButton from "@/components/ClearButton.vue";
 import StartButton from "@/components/StartButton.vue";
 import PersonSearch from "@/components/PersonSearch.vue";
-import {errorHandler} from "@/utils/errorHandler";
+import {handleAxiosError} from "@/utils/errorHandler";
 import {useToast} from "vue-toastification";
 
 const toast = useToast();
@@ -71,7 +71,7 @@ onMounted(async () => {
     originalPersonOption.value = [...PersonOption.value];
   } catch (error) {
     console.error("Erro ao inicializar opções de pessoas:", error);
-    errorHandler(error, toast);
+    handleAxiosError(error, toast);
   }
 });
 
@@ -92,7 +92,7 @@ const onPersonSelect = async (selectedPerson) => {
       }
     } catch (error) {
       console.log("Erro ao buscar dispositivos:", error);
-      errorHandler(error, toast);
+      handleAxiosError(error, toast);
     }
   }
 };
@@ -159,12 +159,6 @@ function handleReset() {
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-.error-message {
-  color: red;
-  font-size: 12px;
-  margin-top: 4px;
-}
 
 .filters {
   position: fixed;
