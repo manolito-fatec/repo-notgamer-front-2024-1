89b917e9093c2f72edc4c18a871c4838786a574d
Vinícius Chaves <126925449+ChavesVini@users.noreply.github.com>
2024-10-29 19:59:07 -0300

    :sparkles: feat: (#37)
    implementing dark and light mode in the map

---
+++ src/views/DarkOrLight.vue
@@ -0,0 +1,52 @@
+<template>  
+  <div class="dark-white-mode">
+    <input type="checkbox" id="toggle" @click="$emit('toggleDarkLightMode')"/>
+    <label for="toggle" class="toggle-label">
+      <i class="fas fa-moon moon"></i>
+      <i class="fas fa-sun sun"></i>
+    </label>
+  </div>
+</template>
+
+<style scoped>
+
+input[type="checkbox"] {
+  display: none;
+}
+
+.toggle-label {
+  cursor: pointer;
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center; 
+  width: 35px;
+  height: 35px;
+}
+
+.sun, .moon {
+  text-align: center;
+  position: absolute;
+  font-size: 35px;
+  transition: 0.3s;
+}
+
+.sun {
+  opacity: 0;
+  color: white;
+}
+
+.moon {
+  opacity: 1;
+  color: black;
+}
+
+input[type="checkbox"]:checked + .toggle-label .sun {
+  opacity: 1;
+}
+
+input[type="checkbox"]:checked + .toggle-label .moon {
+  opacity: 0;
+}
+
+</style>
