6e0f967f15384351b33d0fa861b7cffa9c8d2b38
CauevSilv <caue.silva10@fatec.sp.gov.br>
2024-11-08 00:43:12 -0300

    :sparkles: feat: createVectorLayer new Style

--- src/services/mapService.ts
+++ src/services/mapService.ts
@@ -1,6 +1,7 @@
 import {Feature, Map, View} from "ol";
 import {Tile as TileLayer, Vector as VectorLayer} from "ol/layer";
 import {OSM, Vector as VectorSource, XYZ} from "ol/source";
+import {Fill, Stroke, Style} from "ol/style";
 
 export function createMap(center: number[], zoom: number, projection: string, darkOrWhiteMap?) {
     return new Map({
@@ -25,20 +26,30 @@ export function createNewVectorLayer(featureArray?: Feature[], layername?: strin
             return new VectorLayer({
                 source: drawSource,
                 properties: {layerName: layername},
+                zIndex:1
         })
     } else {
     return new VectorLayer({
         source: new VectorSource({
             features: featureArray,
         }),
+        style: new Style({
+            fill: new Fill({
+                color: 'rgba(255, 0, 0)'
+            }),
+            stroke: new Stroke({
+                color: '#000000',
+                width: 1
+            })
+        }),
         properties: {layerName: layername},
-        zIndex: 50,
+        zIndex: 2,
     });
 }}
 export function createTileLayer(layername?: string, darkOrWhiteMap?: string) {
     return new TileLayer({
         source: new XYZ({
-            url: `https://api.maptiler.com/maps/${darkOrWhiteMap}/{z}/{x}/{y}.png?key=eR9oB64MlktZG90QwIJ7`
+            url: `https://api.maptiler.com/maps/${darkOrWhiteMap}/{z}/{x}/{y}.png?key=DxUujwebq5Zd8hO25SyJ`
         }),
         properties: {layerName: 'TileLayer'},
     })
