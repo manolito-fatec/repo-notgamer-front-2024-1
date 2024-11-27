b39739ba61dfd7ddab88e42ffbb5757806ffbe23
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2024-11-26 21:06:44 -0300

    :construction:feat: crete route to login route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,10 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
